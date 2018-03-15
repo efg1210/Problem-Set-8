a7c0ef61d08a99a13fedaf0ff10ea78a4a9ac993
C9 <egranville@ucvts.org>
2018-03-15 20:27:01 +0000

    Added to homepage js

--- js/homepage-js.js
+++ js/homepage-js.js
@@ -1,5 +1,22 @@
+
 function change(clicked){
     if(clicked == "c"){
-        getElementById("c-tab-btn").style.backgroundColor = "#42dff4";
+        document.getElementById("c-tab-btn").style.backgroundColor = "#42dff4";
+        document.getElementById("v-tab-btn").style.backgroundColor = "white";
+        hide("v-tab");
+        show("c-tab");
+    }else if(clicked == "v"){
+        document.getElementById("v-tab-btn").style.backgroundColor = "#42dff4";
+        document.getElementById("c-tab-btn").style.backgroundColor = "white";
+        hide("c-tab");
+        show("v-tab");
     }
+}
+
+function hide(id){
+    document.getElementById(id).style.display = "none"
+}
+
+function show(id){
+    document.getElementById(id).style.display = "block"
 }
