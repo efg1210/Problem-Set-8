7822eb1ef488a35c304c2843385163c4c4ed378a
Emily Granville <egranville@ucvts.tec.nj.us>
2018-03-15 13:06:31 -0400

    Added cipher page and javascript

---
+++ js/cipher-js.js
@@ -0,0 +1,34 @@
+function caesarEncrypt(key) {
+    var message = document.getElementById('input-box')
+    var encryptedResult = "";
+
+    for(var i = 0; i < message.length; i++)
+    {
+        // Get the character in the original message
+        var originalCharacter = message.charAt(i);
+
+        // If it's an alphabetical character, we'll compute the new
+        // shifted character and add it to the encrypted result
+        var alphabeticIndex = ALPHABET.indexOf(originalCharacter);
+        if(alphabeticIndex >= 0)
+        {
+            // Compute new index
+            var newIndex = alphabeticIndex + key;
+            newIndex = newIndex % ALPHABET.length;
+
+            // Get the new character
+            var newCharacter = ALPHABET.charAt(newIndex);
+
+            // Add the new shifted character to the encrypted result
+            encryptedResult += newCharacter
+        }
+
+        // Otherwise we'll keep the original character
+        else
+        {
+            encryptedResult += originalCharacter;
+        }
+    }
+
+    return encryptedResult;
+}
