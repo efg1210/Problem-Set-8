98887e1809ebe51839cb33847ba61eaaf2aeae86
Emily Granville <egranville@ucvts.tec.nj.us>
2018-03-19 13:23:03 -0400

    Started Vigenere

--- js/cipher-js.js
+++ js/cipher-js.js
@@ -9,12 +9,22 @@ function submit1(e){
   var message = String(e.target.message.value);
   var type = e.target.type.value;
   var direction = e.target.direction.value;
-  var key = parseInt(prompt("Key:"));
+  var key = prompt("Key:");
 
   if(type == "c" && direction == "encode"){
-    showResult(caesarEncrypt(message, key));
-  }else if(type == "c" && direction =="decode"){
-    showResult(caesarDecrypt(message, key))
+    key = parseInt(key);
+    if(!isNaN(key)){
+      showResult(caesarEncrypt(message, key));
+    }
+  }else if(type == "c" && direction == "decode"){
+    key = parseInt(key);
+    if(!isNaN(key)){
+      showResult(caesarDecrypt(message, key));
+    }
+  }else if(type == "v" && direction == "encode"){
+    if(isNaN(parseInt(key))){
+      showResult(vigEncrypt(message, key));
+    }
   }
 }
 
@@ -103,6 +113,49 @@ function caesarDecrypt(message, key) {
         }
     }
     return encryptedResult;
+}
+
+function vigEncrypt(message, key){
+  
+
+  for(var i = 0; i < message.length; i++)
+  {
+      // Get the character in the original message
+      var originalCharacter = message.charAt(i);
+
+      // If it's an alphabetical character, we'll compute the new
+      // shifted character and add it to the encrypted result
+      if(ALPHABET.indexOf(originalCharacter) > 0){
+        var alphabeticIndex = ALPHABET.indexOf(originalCharacter);
+        if(alphabeticIndex >= 0){
+            // Compute new index
+            var newIndex = alphabeticIndex + key;
+            newIndex = newIndex % ALPHABET.length;
+
+            // Get the new character
+            var newCharacter = ALPHABET.charAt(newIndex);
+
+            // Add the new shifted character to the encrypted result
+            encryptedResult += newCharacter
+        }
+      }else if(alphabet.indexOf(originalCharacter) > 0){
+        var alphabeticIndex = alphabet.indexOf(originalCharacter);
+        if(alphabeticIndex >= 0){
+            // Compute new index
+            var newIndex = alphabeticIndex + key;
+            newIndex = newIndex % alphabet.length;
+
+            // Get the new character
+            var newCharacter = alphabet.charAt(newIndex);
+
+            // Add the new shifted character to the encrypted result
+            encryptedResult += newCharacter
+        }
+      }else{
+        encryptedResult += originalCharacter;
+      }
+  }
+  return encryptedResult;
 }
 
 function showResult(result){
